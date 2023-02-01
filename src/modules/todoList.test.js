c4112df1ac73eb420eb8508eecda72cfeda55b86
JakariaJishan <jakariajishan987@gmail.com>
2023-02-02 04:08:30 +0600

    Fix linter errors

--- src/modules/todoList.test.js
+++ src/modules/todoList.test.js
@@ -2,46 +2,44 @@
  * @jest-environment jsdom
  */
 import TaskCollection from './todoList.js';
-const task = new TaskCollection()
 
-describe("Testing functions", () => {
-    // addtask
-    test('Check addTodo able add todo to todoList', () => {
-        document.body.innerHTML = `
+const task = new TaskCollection();
+
+describe('Testing functions', () => {
+  // addtask
+  test('Check addTodo able add todo to todoList', () => {
+    document.body.innerHTML = `
             <input id="txtNewTask" />
             <button id="btnAddTask">Add Task</button>
             <ul id="ulTaskList"></ul>
         `;
-    
-        const newTodoInput = document.getElementById('txtNewTask');
-        const todolist = document.querySelectorAll('#ulTaskList');
-    
-        task.addTask('task 1');
-        let aux ='';
-        task.getTasks().forEach((newTask) => {
-            if (!newTask.completed) {
-                aux += `<li>${newTask.description}</li>`;
-            }
-        });
 
-        todolist.innerHTML = aux;
-        expect(todolist.innerHTML).toBe('<li>task 1</li>');
+    const todolist = document.querySelectorAll('#ulTaskList');
+
+    task.addTask('task 1');
+    let aux = '';
+    task.getTasks().forEach((newTask) => {
+      if (!newTask.completed) {
+        aux += `<li>${newTask.description}</li>`;
+      }
     });
 
-    //removeTask
-    test('Check removeTask ', () => {  
-        const todolist = document.querySelectorAll('#ulTaskList');
-        task.removeTask(0);
-
-        let aux ='';
-        task.getTasks().forEach((newTask) => {
-            if (!newTask.completed) {
-                aux += `<li>${newTask.description}</li>`;
-            }
-        });
-        todolist.innerHTML = aux;
-        expect(todolist.innerHTML).toBe('');
-    });        
+    todolist.innerHTML = aux;
+    expect(todolist.innerHTML).toBe('<li>task 1</li>');
   });
 
+  // removeTask
+  test('Check removeTask ', () => {
+    const todolist = document.querySelectorAll('#ulTaskList');
+    task.removeTask(0);
 
+    let aux = '';
+    task.getTasks().forEach((newTask) => {
+      if (!newTask.completed) {
+        aux += `<li>${newTask.description}</li>`;
+      }
+    });
+    todolist.innerHTML = aux;
+    expect(todolist.innerHTML).toBe('');
+  });
+});
