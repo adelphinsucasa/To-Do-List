e8bce625fb022ec1b07ce73c731798d4c68aa65a
josedelphin <dalejandro.005@gmail.com>
2023-02-01 18:01:22 -0400

    Testing remove function

--- src/modules/todoList.js
+++ src/modules/todoList.js
@@ -1,9 +1,48 @@
-const addTask = () => {
-    const newTodoInput = document.getElementById('txtNewTask');
-    let currentTodoList = document.getElementById('ulTaskList').innerHTML;
-    currentTodoList += `<li>${newTodoInput.value}</li>`;
-    document.getElementById('ulTaskList').innerHTML = currentTodoList;
-    newTodoInput.value = '';
+export default class TaskCollection {
+  constructor() {
+    this.tasks = [];
   }
 
-module.exports = addTask;
+    addTask = (desc) => {
+      const newTask = {
+        description: desc,
+        completed: false,
+        index: this.tasks.length + 1,
+      };
+      this.tasks.push(newTask);
+    }
+
+    removeTask = (index) => {
+      const result = this.tasks.filter((task, i) => i !== index);
+      for (let i = index; i < result.length; i += 1) {
+        result[i].index -= 1;
+      }
+      this.tasks = result;
+    }
+
+    removeAll = () => {
+      this.tasks = [];
+    }
+
+    editTask = (desc, index) => {
+      this.tasks[index].description = desc;
+    }
+
+    clearAllCompleted = () => {
+      const result = this.tasks.filter((task) => !task.completed);
+      for (let i = 0; i < result.length; i += 1) {
+        result[i].index = i + 1;
+      }
+      this.tasks = result;
+    }
+
+    setCompleted = (index, value) => {
+      this.tasks[index].completed = value;
+    }
+
+    getTasks = () => this.tasks;
+
+    setTasks = (tasks) => {
+      this.tasks = tasks;
+    }
+}
--- src/modules/todoList.test.js
+++ src/modules/todoList.test.js
@@ -1,22 +1,47 @@
 /**
  * @jest-environment jsdom
  */
-import TaskCollection from './taskCollection.js';
+import TaskCollection from './todoList.js';
 const task = new TaskCollection()
 
-test('Check addTodo able add todo to todoList', () => {
-    document.body.innerHTML = `
-        <input id="txtNewTask" />
-        <button id="btnAddTask">Add Task</button>
-        <ul id="ulTaskList">
-        </ul>
-    `;
-
-    const newTodoInput = document.getElementById('txtNewTask');
-    const todolist = document.querySelectorAll('#ulTaskList');
-
-    newTodoInput.value = 'New Task!';
+describe("Testing functions", () => {
+    // addtask
+    test('Check addTodo able add todo to todoList', () => {
+        document.body.innerHTML = `
+            <input id="txtNewTask" />
+            <button id="btnAddTask">Add Task</button>
+            <ul id="ulTaskList"></ul>
+        `;
+    
+        const newTodoInput = document.getElementById('txtNewTask');
+        const todolist = document.querySelectorAll('#ulTaskList');
+    
         task.addTask('task 1');
-        todolist.innerHTML = `<li>${task.getTasks()[0].description}</li>`;
-    expect(todolist).toHaveLength(1);
-});
+        let aux ='';
+        task.getTasks().forEach((newTask) => {
+            if (!newTask.completed) {
+                aux += `<li>${newTask.description}</li>`;
+            }
+        });
+
+        todolist.innerHTML = aux;
+        expect(todolist.innerHTML).toBe('<li>task 1</li>');
+    });
+
+    //removeTask
+    test('Check removeTask ', () => {  
+        const todolist = document.querySelectorAll('#ulTaskList');
+        task.removeTask(0);
+
+        let aux ='';
+        task.getTasks().forEach((newTask) => {
+            if (!newTask.completed) {
+                aux += `<li>${newTask.description}</li>`;
+            }
+        });
+        todolist.innerHTML = aux;
+        expect(todolist.innerHTML).toBe('');
+    });        
+  });
+
+
