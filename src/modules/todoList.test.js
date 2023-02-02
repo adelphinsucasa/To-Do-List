36e58c16b04f90bd8e3afac2468fb6b3ad32443f
JakariaJishan <jakariajishan987@gmail.com>
2023-02-02 15:29:11 +0600

    Add remove test

--- src/modules/todoList.test.js
+++ src/modules/todoList.test.js
@@ -27,19 +27,18 @@ describe('Testing functions', () => {
     todolist.innerHTML = aux;
     expect(todolist.innerHTML).toBe('<li>task 1</li>');
   });
-// removeTask
-test('Check removeTask ', () => {
-  const todolist = document.querySelectorAll('#ulTaskList');
-  task.removeTask(0);
+  // removeTask
+  test('Check removeTask ', () => {
+    const todolist = document.querySelectorAll('#ulTaskList');
+    task.removeTask(0);
 
-  let aux = '';
-  task.getTasks().forEach((newTask) => {
-    if (!newTask.completed) {
-      aux += `<li>${newTask.description}</li>`;
-    }
+    let aux = '';
+    task.getTasks().forEach((newTask) => {
+      if (!newTask.completed) {
+        aux += `<li>${newTask.description}</li>`;
+      }
+    });
+    todolist.innerHTML = aux;
+    expect(todolist.innerHTML).toBe('');
   });
-  todolist.innerHTML = aux;
-  expect(todolist.innerHTML).toBe('');
-});
-  
 });
