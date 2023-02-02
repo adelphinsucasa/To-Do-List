da1392ea98ee8d47a28cdd9b3c5c69b598b58011
josedelphin <dalejandro.005@gmail.com>
2023-02-02 11:30:50 -0400

    Testing Edit Function

--- src/modules/todoList.test.js
+++ src/modules/todoList.test.js
@@ -28,6 +28,22 @@ describe('Testing functions', () => {
     expect(todolist.innerHTML).toBe('<li>task 1</li>');
   });
 
+  //Editing Function
+  test('Check a task edited', () =>{
+    const todolist = document.querySelectorAll('#ulTaskList');
+    task.editTask('Task 1 modified',0);
+
+    let aux = '';
+    task.getTasks().forEach((newTask) => {
+      if (!newTask.completed) {
+        aux += `<li>${newTask.description}</li>`;
+      }
+    });
+    todolist.innerHTML = aux;
+
+    expect(todolist.innerHTML).toBe('<li>Task 1 modified</li>');
+  });
+
   // removeTask
   test('Check removeTask ', () => {
     const todolist = document.querySelectorAll('#ulTaskList');
